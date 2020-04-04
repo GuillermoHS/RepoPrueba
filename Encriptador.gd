1621adf90ebc1eee7282265541148b16de1aa51f
GuillermoHS <guillermo.hernandezsanchez@iesjuandeherrera.net>
2020-04-05 01:57:40 +0200

    Scripts 3

--- Encriptador.gd
+++ Encriptador.gd
@@ -4,13 +4,10 @@ const DICCIONARIO = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","ñ
 
 func _ready():
 	pass
-<<<<<<< HEAD
+
 # Funcion para volver a la escena de inicio
 func _on_Volver_pressed():
 	get_tree().change_scene("res://Inicio.tscn")
-=======
-
-
 
 func _on_Boton_Encriptar_pressed():
 	if $TextEdit.text == "":
@@ -18,7 +15,10 @@ func _on_Boton_Encriptar_pressed():
 		pass
 	var clave = $TextEdit
 	var claveArray = []
+	var clavePublica: int = 0
 	for c in clave:
 		claveArray.append(c)
+		for p in DICCIONARIO.size():
+			if claveArray[c] == DICCIONARIO[p]:
+				clavePublica = clavePublica + p
 	
->>>>>>> efa2f880d3e836e22fb22c013fefb0b92e83ad6e
