c8cf8237f8a2333ae2c9eebbd5b471dd8eff2a36
AlesCordero <alejandro.corderodiadosa@iesjuandeherrera.net>
2020-04-05 19:44:48 +0200

    Descodificador

--- Desencriptador.gd
+++ Desencriptador.gd
@@ -1,15 +1,19 @@
 extends CanvasLayer
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func _on_Boton_Encriptar_pressed():
+	if $TexoAdesencriptar.text == "":
+		$AcceptDialog.dialog_text = "No se ha introducido texto para desencriptar"
+		$AcceptDialog.show()
+	if $Clavep.text == "":
+		$AcceptDialog.dialog_text = "No se ha introducido una clave"
+		$AcceptDialog.show()
 
 # Funcion para volver a la escena de inicio
 func _on_Volver_pressed():
 	get_tree().change_scene("res://Inicio.tscn")
-
-
+# Funcion para resetear los textos
 func _on_ButtonDES_pressed():
 	$TexoAdesencriptar.text = ""
 	$TextoDesencriptado.text = ""
 	$ClaveP.text = ""
+
